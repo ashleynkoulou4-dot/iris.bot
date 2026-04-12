117080acf3db9288ed70b5920cf5e927b08a58aa
ashleynkoulou4-dot <ashleynkoulou4@gmail.com>
2026-04-12 20:39:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const truthOrDare = require('./commands/truth_or_dare')
 const opening = require('./commands/opening')
 const news = require('./commands/news')
 const quiz = require('./commands/quiz')
+if (text.toLowerCase() === '!quiz') await quiz.startQuiz(sock, msg)
+else if (text.toLowerCase().startsWith('!rep')) {
+  const answer = text.replace('!rep', '').trim()
+  await quiz.checkAnswer(sock, msg, answer)
+}
+else if (text.toLowerCase() === '!ranking') await quiz.showRanking(sock, msg)
+
 
 // Dans messages.upsert :
 
