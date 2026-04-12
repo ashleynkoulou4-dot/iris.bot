09264d080090ebde4107458ba5c64aa40c6e50a2
ashleynkoulou4-dot <ashleynkoulou4@gmail.com>
2026-04-12 20:54:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ const compatibility = require('./commands/compatibility')
 const truthOrDare = require('./commands/truth_or_dare')
 const opening = require('./commands/opening')
 const news = require('./commands/news')
-const quiz = require('./commands/quiz')
-if (text.toLowerCase() === '!quiz') await quiz.startQuiz(sock, msg)
-else if (text.toLowerCase().startsWith('!rep')) {
-  const answer = text.replace('!rep', '').trim()
-  await quiz.checkAnswer(sock, msg, answer)
-}
-else if (text.toLowerCase() === '!ranking') await quiz.showRanking(sock, msg)
-
-
-// Dans messages.upsert :
-
-else if (text.toLowerCase().startsWith('!rep')) {
-    const answer = text.replace('!rep', '').trim()
-    await quiz.checkAnswer(sock, msg, answer)
-}
-else if (text.toLowerCase() === '!ranking') await quiz.showRanking(sock, msg)
-
 
 // Dans messages.upsert :
 if (text.toLowerCase() === '!quiz') await quiz(sock, msg)
